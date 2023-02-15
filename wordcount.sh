6e2de0b52f0799f63280f4f1b4cf6bc1690f8a02
peiprjs <peipr@peiphy.xyz>
2023-02-15 13:17:27 +0100

    Added a wordcount tool

---
+++ wordcount.sh
@@ -0,0 +1,12 @@
+echo "all"
+detex main.tex | wc
+echo "Chapter 0"
+detex chapters/intro.tex | wc
+echo "Chapter 1"
+detex chapters/chapter01.tex | wc
+echo "Chapter 2"
+detex chapters/chapter02.tex | wc
+echo "Chapter 3"
+detex chapters/chapter03.tex | wc
+echo "Chapter 4"
+detex chapters/chapter04.tex | wc
