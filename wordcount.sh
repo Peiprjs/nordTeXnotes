d9a0401d3268dff82975193d337f1376ab14c8a1
peiprjs <peipr@peiphy.xyz>
2023-02-26 18:51:49 +0100

    Spellcheck!

--- wordcount.sh
+++ wordcount.sh
@@ -1,12 +1,12 @@
 echo "all"
 detex main.tex | wc
-echo "-171 words, footnotes"
 echo "Chapter 0"
 detex chapters/intro.tex | wc
 echo "Chapter 1"
 detex chapters/chapter01.tex | wc
+echo "Chapter 1.5"
+detex chapters/expdes.tex | wc
 echo "Chapter 2"
 detex chapters/chapter02.tex | wc
-
 echo "Chapter 4"
 detex chapters/chapter04.tex | wc
